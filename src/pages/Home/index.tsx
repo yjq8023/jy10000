0dcbc4bce2b91667544402cd088a5accd999bf64
yangjiqiang <1005473728@qq.com>
2022-04-06 14:47:33 +0800

    feat: 引用蝴蝶ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { Layout } from '@sinohealth/butterfly-ui-components/lib';
 
 function Home() {
+  const menuData = [
+    {
+      label: '一级菜单',
+      key: '1',
+      icon: '',
+      children: [
+        {
+          label: '二级菜单',
+          key: '1-1',
+          children: [],
+        },
+      ],
+    },
+    {
+      label: '一级菜单2',
+      key: '2',
+      icon: '',
+      children: [
+        {
+          label: '二级菜单2',
+          key: '2-1',
+          children: [],
+        },
+      ],
+    },
+  ];
+  const menuData2 = [
+    {
+      label: '首页', key: 'home', icon: '', children: [],
+    },
+    {
+      label: '首页2', key: 'home2', icon: '', children: [],
+    },
+  ];
+  const menuConfig = {
+    sideMenu: menuData,
+    headerMenu: menuData2,
+    sideMenuSelectedKeys: ['1-1'],
+    headerMenuSelectedKeys: [menuData2[0].key],
+  };
+  const logo = (<div>中康患者管理平台</div>);
+  const toolbar = (<div>用户信息</div>);
   return (
     <div>
-      <h1>Home11234</h1>
-      <div>
-        <Link to="page1">to page11</Link>
-        <br />
-        <Link to="page2">to page2</Link>
-        <br />
-        <Link to="login">to login</Link>
-      </div>
-      <div>
+      {/* @ts-ignore */}
+      <Layout menuConfig={menuConfig} collapsed={false} logo={logo} toolbar={toolbar}>
         <Outlet />
-      </div>
+      </Layout>
     </div>
   );
 }
