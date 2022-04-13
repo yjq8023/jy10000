3962ea950b7f1a52c89309ed9c4ce793b1576951
yangjiqiang <1005473728@qq.com>
2022-04-13 10:52:58 +0800

    feat: 添加请求成功和错误处理逻辑

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,14 +1,15 @@
 /* eslint no-undef: 0 */
 import axios from 'axios';
 import qs from 'qs';
+import { message } from '@sinohealth/butterfly-ui-components/lib';
 import { getToken } from '@/utils/cookies';
 
 // 默认配置
 axios.defaults.timeout = 15000;
 axios.defaults.withCredentials = true;
 // @ts-ignore
-// axios.defaults.headers['Content-Type'] = 'application/json';
-axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers['Content-Type'] = 'application/json';
+// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 const { NODE_ENV } = process.env;
 const baseURL = NODE_ENV === 'development' ? 'http://192.168.16.58:8805' : '/';
 // 普通请求实例
@@ -43,17 +44,23 @@ function beforeRequest(options: any) {
   options.headers.Authorization = getToken();
   return options;
 }
-function resolve(data: any) {
-  return data.data;
+function resolve(response: any) {
+  const { data } = response;
+  if (data.code === 200) {
+    return Promise.resolve(data.result);
+  }
+  message.error(`Server Error: ${data.message || '服务器出错了，请稍后再试！'}`);
+  return Promise.reject(data);
 }
 function reject(error: any) {
-  let { message } = error;
+  let errorMessageText = error.message;
   if (error.response) {
-    message = error.response.data;
+    errorMessageText = error.response.data && error.response.data.message;
   } else if (error.request) {
-    message = error.request;
+    errorMessageText = error.request;
   }
-  console.error(message);
+  message.error(`Server error: ${errorMessageText || '服务器出错了，请稍后再试！'}`);
+  return Promise.reject(error);
 }
 request.interceptors.request.use(beforeRequest);
 request.interceptors.response.use(resolve, reject);
