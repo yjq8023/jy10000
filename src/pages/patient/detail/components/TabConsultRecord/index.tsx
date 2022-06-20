c262a6eb059b50a26e72c63506a72329db49c16e
yangjiqiang <1005473728@qq.com>
2022-06-20 15:49:24 +0800

    feat: 咨询记录临时保存

--- src/pages/patient/detail/components/TabConsultRecord/index.tsx
+++ src/pages/patient/detail/components/TabConsultRecord/index.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Dropdown, Menu } from '@sinohealth/butterfly-ui-components/lib';
+
+import style from './index.less';
 
 function TabConsultRecord() {
+  const items: any = [
+    {
+      key: '1',
+      label: (
+        <a>
+          全部
+        </a>
+      ),
+    },
+  ];
+  const menu = (
+    // @ts-ignore
+    <Menu items={items} />
+  );
   return (
-    <div>TabConsultRecord</div>
+    <div className={style.consultRecord}>
+      <div className={style.consultRecordList}>
+        <div className="but-title">
+          患者咨询记录
+          <Dropdown overlay={menu}>
+            <a onClick={(e) => e.preventDefault()}>
+              选择项目
+            </a>
+          </Dropdown>
+        </div>
+      </div>
+      <div className={style.consultRecordDetail}>
+        123
+      </div>
+    </div>
   );
 }
 
