f3a9e6ba9e447f95ff52b838c5f938df2d849d33
yangjiqiang <1005473728@qq.com>
2022-06-24 15:29:03 +0800

    feat: 配置修改

--- src/config/base.ts
+++ src/config/base.ts
@@ -1,16 +1,12 @@
 const { NODE_ENV } = process.env;
 export const baseURL = NODE_ENV === 'development' ? 'https://backend-hccm-dev.zmnyun.cn/' : '/';
 export const scope = 'sdc-hccm';
-export const socketURL =
-  NODE_ENV === 'development'
-    ? 'ws://dev.sdc.sinohealth.cn:30200/webSocket'
-    : `ws://${window.location.hostname}:30200/webSocket`;
 export const getTokenParams = {
   clientId: 'healthplus-oms',
   clientSecret: 'healthplus-oms',
   scope: 'zmn-rx-oms-server',
 };
-export const loginRememberKey = 'zk-r-u'; // 记住密码时保存数据的key
+export const loginRememberKey = 'zk-hccm-w'; // 记住密码时保存数据的key
 export default {
   baseURL,
   getTokenParams,
