42335a40885101fefcbe89e7a71a2dcc9c414a28
likaifeng <lkfeng2016@163.com>
2022-08-16 15:48:52 +0800

    feat: 上传封面组件、标签选择组件

---
+++ src/pages/project/articleLibrary/components/UploadCover/index.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { message, Upload, UploadProps } from '@sinohealth/butterfly-ui-components/lib';
+import { UploadOutlined, DeleteOutlined } from '@ant-design/icons';
+import { baseURL, scope } from '@/config/base';
+import styles from './index.less';
+import { getToken } from '@/utils/cookies';
+import { getBase64 } from '@/utils';
+
+const defaultAccepts = 'image/png, image/jpeg';
+
+interface CustomUploadProps extends UploadProps {
+  fileId?: string;
+  onSuccess: (values: any) => void;
+  onDel: () => void;
+  maxSize: number;
+}
+
+/**
+ * 上传封面
+ * @returns
+ */
+const UploadCover: React.FC<CustomUploadProps> = (props) => {
+  const { maxSize, fileId, onSuccess, onDel } = props;
+  const [imgBase64, setImgBase64] = useState('');
+
+  const uploadProps: UploadProps = {
+    name: 'file',
+    action: `${baseURL}cs/file/public/upload`,
+    headers: {
+      authorization: getToken() || '',
+      scope,
+      app: 'sdc-hccm-backend',
+    },
+    itemRender() {
+      return '';
+    },
+    accept: defaultAccepts,
+    onRemove: () => {},
+    onChange(info: any) {
+      if (info.file.status === 'done') {
+        getBase64(info.file.originFileObj, (imageUrl: string) => {
+          setImgBase64(imageUrl);
+        });
+        onSuccess(info.file.response.data);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} 上传失败`);
+      }
+    },
+    beforeUpload: async (file: any) => {
+      const accepts = defaultAccepts;
+      if (accepts.indexOf(file.type) === -1) {
+        message.error(`文件类型仅限${accepts}!`);
+        return false;
+      }
+      const isLt1M = file.size / 1024 / 1024 < maxSize;
+      if (!isLt1M) {
+        message.error(`上传图片不能大于${maxSize}MB!`);
+      }
+      return isLt1M;
+    },
+  };
+
+  const handleDelBase64 = () => {
+    setImgBase64('');
+    onDel && onDel();
+  };
+
+  const UploadContainer = () => {
+    return (
+      <div className={styles['upload-container']}>
+        <UploadOutlined
+          style={{ fontSize: 30, color: '#217BA0' }}
+          className={styles['upload-icon']}
+        />
+        <div className={styles['upload-title']}>上传图片</div>
+        <div className={styles['upload-tip']}>
+          最多可上传1张图片，图片支持上传jpg/png格式，单个图片不超过500KB。
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    setImgBase64(fileId || '');
+  }, [fileId]);
+
+  return (
+    <div className={styles['upload-cover']}>
+      {!imgBase64 ? (
+        <Upload {...uploadProps}>{UploadContainer()}</Upload>
+      ) : (
+        <div className={styles['upload-preview']}>
+          <img className={styles['del-img']} src={imgBase64} alt="封面" />
+          <div className={styles.delete} onClick={() => handleDelBase64()}>
+            <DeleteOutlined style={{ fontSize: 24 }} className={styles['del-icon']} />
+            <div className={styles['del-title']}>删除图片</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadCover;
--- src/pages/project/components/LabelSelect/index.tsx
+++ src/pages/project/components/LabelSelect/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Button, Checkbox, Input, Space } from '@sinohealth/butterfly-ui-components/lib';
+import { Button, Checkbox, Input, Popover, Space } from '@sinohealth/butterfly-ui-components/lib';
 import { PlusCircleOutlined, UpOutlined } from '@ant-design/icons';
 import styles from './index.less';
 import { httpGetLabelList } from '@/services/project';
@@ -7,6 +7,7 @@ import { httpGetLabelList } from '@/services/project';
 const SearchIcon = () => <div className={`${styles['search-icon']} iconfont icon-search`} />;
 
 type LabelSelectProps = {
+  mapSour?: string[];
   search?: boolean;
   add?: boolean;
   placeholder?: string;
@@ -18,7 +19,8 @@ type LabelSelectProps = {
  * @returns
  */
 const LabelSelect: React.FC<LabelSelectProps> = (props) => {
-  const { search = true, add = true, onSelect, placeholder = '请输入' } = props;
+  const { mapSour, search = true, add = true, onSelect, placeholder = '请输入' } = props;
+  const [isMap, setIsMap] = useState(false);
   const [selected, setSelected] = useState<ProjectType.LabelListRes[]>([]);
   const [source, setSource] = useState<ProjectType.LabelListRes[]>([]);
 
@@ -46,18 +48,36 @@ const LabelSelect: React.FC<LabelSelectProps> = (props) => {
     const res: any = await httpGetLabelList({});
     console.log(res);
     if (!res.empty) {
-      checkedDefault(res.data);
+      checkedDefault(res.data, true);
     }
   };
 
-  const checkedDefault = (arr: any) => {
-    arr.forEach((el: any) => {
-      el.children.forEach((lis: any) => {
-        // eslint-disable-next-line no-param-reassign
-        lis.checked = false;
+  const checkedDefault = (arr: any, filter: boolean = false) => {
+    const FilD: ProjectType.LabelListRes[] = [];
+    arr.forEach((el: ProjectType.LabelListRes) => {
+      el.children.forEach((lis: ProjectType.LabelListRes) => {
+        if (filter) {
+          mapSour?.includes(lis.id) && FilD.push(lis);
+          setSelected(FilD);
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          lis.checked = false;
+        }
       });
     });
     setSource(arr);
+  };
+
+  const PopoverContent = () => {
+    return (
+      <div className={styles['popover-content']}>
+        {selected.map((el) => (
+          <span className={styles['select-tag']} key={el.id}>
+            {el.value}
+          </span>
+        ))}
+      </div>
+    );
   };
 
   const clickCallback = (event: { target: any }) => {
@@ -84,31 +104,54 @@ const LabelSelect: React.FC<LabelSelectProps> = (props) => {
     httpGetLabelListReq();
   }, []);
 
+  useEffect(() => {
+    if (isMap || !mapSour?.length) return;
+
+    const FilD: ProjectType.LabelListRes[] = [];
+    source.forEach((el: ProjectType.LabelListRes) => {
+      el.children.forEach((lis: ProjectType.LabelListRes) => {
+        if (mapSour?.includes(lis.id)) {
+          // eslint-disable-next-line no-param-reassign
+          lis.checked = true;
+          FilD.push(lis);
+          setSelected(FilD);
+        }
+      });
+      setIsMap(true);
+    });
+  }, [mapSour, source]);
+
   return (
     <div className={styles['label-select']} ref={inputRef}>
-      <div
-        className={`${styles['label-value']} ${selected.length ? '' : styles.placeholder}`}
-        onClick={() => setIsShowDrop(true)}
-      >
-        {selected.map((el) => (
-          <span className={styles['select-tag']} key={el.id}>
-            {el.value}
-          </span>
-        ))}
-        {!selected.length ? '请输入量表名称' : ''}
-        {!selected.length ? (
-          <span className={`${styles['icon-select']} iconfont icon-arrow-down`} />
-        ) : (
-          <span
-            className={`${styles['icon-select']} iconfont icon-shibai1`}
-            onClick={(e) => {
-              e.stopPropagation();
-              checkedDefault(source);
-              setSelected([]);
-            }}
-          />
-        )}
-      </div>
+      <Popover style={{ width: '320px' }} content={selected.length ? PopoverContent : ''}>
+        <div
+          className={`${styles['label-value']} ${selected.length ? '' : styles.placeholder}`}
+          onClick={() => setIsShowDrop(true)}
+        >
+          {selected.map((el) => (
+            <span className={styles['select-tag']} key={el.id}>
+              {el.value}
+            </span>
+          ))}
+          {!selected.length ? placeholder : ''}
+          {!selected.length &&
+            (!isShowDrop ? (
+              <span className={`${styles['icon-select']} iconfont icon-arrow-down`} />
+            ) : (
+              <span className={`${styles['icon-select']} iconfont icon-arrow-up`} />
+            ))}
+          {selected.length ? (
+            <span
+              className={`${styles['icon-select']} iconfont icon-shibai1`}
+              onClick={(e) => {
+                e.stopPropagation();
+                checkedDefault(source);
+                setSelected([]);
+              }}
+            />
+          ) : null}
+        </div>
+      </Popover>
       {/* <Input
         className={`${styles['label-select-input']} ${styles.placeholder}`}
         placeholder={placeholder}
