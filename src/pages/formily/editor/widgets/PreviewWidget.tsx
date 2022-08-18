4a4514fd7839888848f03de9557d8a95e7e8e373
yangjiqiang <1005473728@qq.com>
2022-08-18 19:00:08 +0800

    feat: 预览

--- src/pages/formily/editor/widgets/PreviewWidget.tsx
+++ src/pages/formily/editor/widgets/PreviewWidget.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useMemo } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { FormRender, registerComponents } from '@sinohealth/butterfly-formily-engine';
 import { TreeNode } from '@sinohealth/designable-core';
 import { transformToSchema } from '@sinohealth/designable-formily-transformer';
@@ -16,17 +16,32 @@ export interface IPreviewWidgetProps {
 
 export const PreviewWidget: React.FC<IPreviewWidgetProps> = (props) => {
   const { form: formProps, schema } = transformToSchema(props.tree);
-  console.log('formProps');
-  console.log(formProps);
+  const [isPc, setIsPC] = useState(true);
+  const dom = useRef();
+  const iframe = useRef();
+  const mobileSiteUrl = 'http://localhost:10086/#/pages/formily/index';
+  useEffect(() => {
+    if (dom.current) {
+      setIsPC(dom.current.offsetWidth > 420);
+    }
+  }, []);
+  useEffect(() => {
+    if (!isPc && iframe.current) {
+      const win = iframe.current.contentWindow;
+      iframe.current.onload = () => {
+        win.postMessage(JSON.stringify({ formProps, schema }), '*');
+      };
+    }
+  }, [isPc, iframe]);
   return (
-    <div style={{ padding: '8px' }}>
-      <div>
-        <Radio.Group defaultValue="a">
-          <Radio.Button value="a">PC</Radio.Button>
-          <Radio.Button value="b">移动</Radio.Button>
-        </Radio.Group>
-      </div>
-      <FormRender schema={schema} formProps={{ componentProps: formProps }} components={components} />
+    <div style={{ width: '100%', height: '100%' }} ref={dom}>
+      { isPc && (
+        <FormRender schema={schema} formProps={{ componentProps: formProps }} components={components} />
+      )}
+      { !isPc && (
+        // eslint-disable-next-line jsx-a11y/iframe-has-title
+        <iframe ref={iframe} src={mobileSiteUrl} frameBorder="0" style={{ width: '100%', height: '100%' }} />
+      )}
     </div>
   );
 };
