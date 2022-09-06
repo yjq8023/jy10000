896350facbff7f37aede5263363c11f5f6a9ddaa
yangjiqiang <1005473728@qq.com>
2022-09-06 15:08:18 +0800

    feat: 更新移动端预览site

--- public/h5/chunk/18.js
+++ public/h5/chunk/18.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{"67":function(n,t,o){"use strict";o.r(t),o.d(t,"taro_block_core",(function(){return c}));var e=o(31),c=function(){function r(n){Object(e.g)(this,n)}return r.prototype.render=function(){return Object(e.e)(e.a,null)},r}()}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{"66":function(n,t,o){"use strict";o.r(t),o.d(t,"taro_block_core",(function(){return c}));var e=o(31),c=function(){function r(n){Object(e.g)(this,n)}return r.prototype.render=function(){return Object(e.e)(e.a,null)},r}()}}]);
